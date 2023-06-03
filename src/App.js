e88fad45ebdffd2cba073df6e940991fa4d26cb0
DikshakAdhikari <ddikshakk@gmail.com>
2023-06-03 21:42:39 +0530

    Dropdown countries and cities

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const countries= [
+  {name:'India', value: 'IN', cities: ['Delhi','Mumbai'] },
+  {name:'Pak', value: 'PK', cities: ['Lahore','Karachi'] },
+  {name:'Bangladesh', value: 'BG', cities: ['Dhaka','Chittagong'] },
+]
+
+
+
+function App() {
+
+  const [getState, setState] = useState([{  name:"", value:"", cities:[]  } ] );
+
+  const handle= (num)=> {
+    console.log(num.target.value);
+    setState(num.target.value);
+  }
+  
+  return (
+    <div className="App">
+      <div>
+    {/* Dropdown 1 */}
+            <select onChange= {handle}>
+        {countries.map((val, index)=> (
+              <option value={index} key={val.value} > 
+              {val.name}
+              </option>   
+         
+            ))}
+              </select>  
+          
+       {/*Dropdown 2*/}
+
+       <select>
+          {countries[getState].cities.map((tar)=> (
+            <option>
+                {tar}
+            </option>
+          ))}
+       </select>
+
+      </div>
+        
+    </div>
+  );
+}
+
+export default App;
